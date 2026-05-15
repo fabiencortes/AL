9374a66972d223e65273516734010aa7f195098a
fabiencortes <fabien.cortes@outlook.com>
2026-05-15 18:23:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -371,7 +371,6 @@ USERS = {
     "ge": {"password": "ge", "role": "driver", "chauffeur_code": "GE"},
     "lillo": {"password": "lillo", "role": "driver", "chauffeur_code": "LILLO"},
     "jf": {"password": "jf", "role": "driver", "chauffeur_code": "JF"},
-    "jef": {"password": "jef", "role": "driver", "chauffeur_code": "JEF"},
 }
 
 # Fallback si Feuil2 ne contient rien
